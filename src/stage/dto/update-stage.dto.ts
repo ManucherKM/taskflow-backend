ab0c1528e70ecbb7a7999032124571a1ca77275a
ManucherKM <manucher20056@gmail.com>
2024-02-07 23:22:57 +0500

    fix bugs

--- src/stage/dto/update-stage.dto.ts
+++ src/stage/dto/update-stage.dto.ts
@@ -1,18 +1,11 @@
 import { PartialType } from '@nestjs/mapped-types'
-import {
-	ArrayMinSize,
-	IsArray,
-	IsNotEmpty,
-	IsOptional,
-	IsString,
-} from 'class-validator'
+import { IsArray, IsNotEmpty, IsOptional, IsString } from 'class-validator'
 import { CreateStageDto } from './create-stage.dto'
 
 export class UpdateStageDto extends PartialType(CreateStageDto) {
 	@IsOptional()
 	@IsArray()
 	@IsString({ each: true })
-	@ArrayMinSize(1)
 	tasks?: string[]
 
 	@IsOptional()
